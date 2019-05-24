329239d19cb31a14c629e87f5df15497cfffe265
Ronny Alvarado <rsalvarado777@gmail.com>
2019-05-23 22:08:27 -0500

    Configured and connected the database using knex and created the get endpoint to retrieve information. Returns empty array with 200 status.

--- index.js
+++ index.js
@@ -1,12 +1,35 @@
-const express = require('express');
-const helmet = require('helmet');
+const express = require("express");
+const helmet = require("helmet");
 
 const server = express();
+
+const knex = require("knex");
+const knexConfig = {
+  client: "sqlite3",
+  connection: {
+    filename: "./data/lambda.sqlite3"
+  },
+  useNullAsDefault: true
+};
+const db = knex(knexConfig);
 
 server.use(express.json());
 server.use(helmet());
 
 // endpoints here
+
+server.get("/api/zoos", (req, res) => {
+  db("zoos")
+    .then(zoo => {
+      res.status(200).json(zoo);
+    })
+    .catch(err => {
+      console.log(err);
+      res
+        .status(400)
+        .json({ message: "Something went wrong with retrieving the data." });
+    });
+});
 
 const port = 3300;
 server.listen(port, function() {
