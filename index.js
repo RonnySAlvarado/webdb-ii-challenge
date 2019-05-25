3de5d781ccb6036cf9abcf1f47e7deb01f1e2552
Ronny Alvarado <rsalvarado777@gmail.com>
2019-05-25 10:13:03 -0500

    Created delete endpoint to delete from the database

--- index.js
+++ index.js
@@ -58,6 +58,21 @@ server.get("/api/zoos/:id", (req, res) => {
     });
 });
 
+server.delete("/api/zoos/:id", (req, res) => {
+  const { id } = req.params;
+  db("zoos")
+    .where({ id })
+    .del()
+    .then(count => {
+      res.status(200).json(count);
+    })
+    .catch(err => {
+      res
+        .status(500)
+        .json({ message: "Something went wrong with this request." });
+    });
+});
+
 const port = 3300;
 server.listen(port, function() {
   console.log(`\n=== Web API Listening on http://localhost:${port} ===\n`);
